5befa3ee2f9d52c5671fffdc2ed62478d2553735
Lucas Tamir Faria <lucastamir@gmail.com>
2023-01-27 08:58:31 -0300

    :lipstick: adding and configuring global style
    global style with styled components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,30 @@
+import GlobalStyles from '@/styles/global'
+// import Document, { DocumentContext } from 'next/document'
+// import { ServerStyleSheet } from 'styled-components'
+
+// class MyDocument extends Document {
+//   static async getInitialProps(ctx: DocumentContext) {
+//     const sheet = new ServerStyleSheet()
+//     const originalRenderPage = ctx.renderPage
+
+//     try {
+//       ctx.renderPage = () =>
+//         originalRenderPage({
+//           enhanceApp: (App) => (props) =>
+//             sheet.collectStyles(<App {...props} />)
+//         })
+
+//       const initialProps = await Document.getInitialProps(ctx)
+//       return {
+//         ...initialProps,
+//         styles: [initialProps.styles, sheet.getStyleElement()]
+//       }
+//     } finally {
+//       sheet.seal()
+//     }
+//   }
+// }
+
 export default function RootLayout({
   children
 }: {
@@ -6,7 +33,12 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <GlobalStyles />
+        {children}
+      </body>
     </html>
   )
 }
+
+// export default { RootLayout, MyDocument }
