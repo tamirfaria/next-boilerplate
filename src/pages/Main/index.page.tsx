29399e66c4d601275453eda1b131a360fb3e44aa
Lucas Tamir Faria <lucastamir@gmail.com>
2023-01-27 13:56:55 -0300

    :lipstick: updating Main page styles

--- src/pages/Main/index.page.tsx
+++ src/pages/Main/index.page.tsx
@@ -1,12 +1,25 @@
 import Contacts from '@/components/Contacts'
 import Image from 'next/image'
+import styled from 'styled-components'
 import styles from './page.module.css'
+
+const Footer = styled.footer`
+  position: absolute;
+  bottom: 10rem;
+`
+const Text = styled.p`
+  font-size: 2rem;
+  color: #808080;
+`
 
 const Main = () => {
   return (
     <main className={styles.main}>
-      <Image src="/logo.png" alt="Logo" width={360} height={360} priority />
+      <Image src="/logo.svg" alt="Logo" width={480} height={480} priority />
       <Contacts />
+      <Footer>
+        <Text>Desenvolvido por Tamir Faria | 2023</Text>
+      </Footer>
     </main>
   )
 }
